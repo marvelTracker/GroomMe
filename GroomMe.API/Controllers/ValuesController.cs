9009ea614e257fdfdacf0b23039a07ef3f840dbf
marveltracker <dineshranasinghe@gmail.com>
2017-07-10 15:46:03 +1000

    Added log details

--- GroomMe.API/Controllers/ValuesController.cs
+++ GroomMe.API/Controllers/ValuesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -10,10 +11,16 @@ namespace Web.API.Kata.Controllers
     //[Authorize]
     public class ValuesController : ApiController
     {
+        private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         // GET api/values
         public IEnumerable<string> Get()
         {
+            Log.Info("Get Value Start");
+
             return new string[] { "value1", "value2" };
+
+            Log.Info("Get Value End");
         }
 
         // GET api/values/5
